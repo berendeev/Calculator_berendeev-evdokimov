03629372406f399d0168c844896cc604d9c40cc4
berendeev <sellect.47@gmail.com>
2017-07-04 16:30:07 +0600

    Implemented the factory pattern

---
+++ Calculator/Calculator/Factory.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class Factory
+    {
+        public static IOperations CreateCalculator(string name)
+        {
+            switch (name)
+            {
+                case "btn_Plus":
+                    return new Addition();
+                    break;
+                case "btn_Minus":
+                    return new Subtraction();
+                    break;
+                case "btn_Multiply":
+                    return new Multiplication();
+                    break;
+                case "btn_Divide":
+                    return new Division();
+                    break;
+                default:
+                    throw new Exception("Неизвестная операция");
+            }
+        }
+    }
+}
--- Calculator/Calculator/Form1.cs
+++ Calculator/Calculator/Form1.cs
@@ -22,23 +22,10 @@ namespace Calculator
         {
             double numberOne = Convert.ToDouble(tb_InputOne.Text);
             double numberTwo = Convert.ToDouble(tb_InputTwo.Text);
-            switch (((Button)sender).Name)
-            {
-                case "btn_Plus":
-                    lbl_Equally.Text = Convert.ToString(numberOne + numberTwo);
-                break;
-                case "btn_Minus":
-                    lbl_Equally.Text = Convert.ToString(numberOne - numberTwo);
-                break;
-                case "btn_Multiply":
-                    lbl_Equally.Text = Convert.ToString(numberOne * numberTwo);
-                break;
-                case "btn_Divide":
-                    lbl_Equally.Text = Convert.ToString(numberOne / numberTwo);
-                break;
-                default:
-                    throw new Exception("Неизвестная операция");
-            }
+            IOperations calculator = Factory.CreateCalculator(((Button)sender).Name);
+            lbl_Equally.Text = Convert.ToString(calculator.Calculate(numberOne, numberTwo));
+            
         }
     }
+
 }
