b826159218a56820cfdbf2d6caa8602855f4f29e
berendeev <sellect.47@gmail.com>
2017-07-05 19:26:17 +0600

    add test for Arccos

---
+++ Calculator/Calculator.Tests/OneArgument/ArccosTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.OneArgument;
+using NUnit.Framework;
+
+namespace Calculator.Tests.OneArgument
+{
+    public class ArccosTest
+    {
+        [TestCase(1,0)]
+        [TestCase(-1,Math.PI)]
+        [TestCase(0.1, 1.47)]
+        public void CalculateTest(double Argument, double expected)
+        {
+            var calculate = new Arccos();
+            double result = calculate.Calculate(Argument);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
