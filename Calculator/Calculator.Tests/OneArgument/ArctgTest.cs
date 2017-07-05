986eb769e7db583fe42e17af46278cc9b00ae500
berendeev <sellect.47@gmail.com>
2017-07-05 19:38:39 +0600

    add test for Arctg

---
+++ Calculator/Calculator.Tests/OneArgument/ArctgTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Calculator.OneArgument;
+using NUnit.Framework;
+
+namespace Calculator.Tests.OneArgument
+{
+    public class ArctgTest
+    {
+        [TestCase(0.5, 0.46)]
+        [TestCase(1, 0.78)]
+        [TestCase(0, 0)]
+        public void CalculateTest(double Argument, double expected)
+        {
+            var calculator = new Arctg();
+            double result = calculator.Calculate(Argument);
+            Assert.AreEqual(expected, result, 0.01);
+        }
+    }
+}
--- Calculator/Calculator/OneArgument/Arctg.cs
+++ Calculator/Calculator/OneArgument/Arctg.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Calculator.OneArgument
 {
-    class Arctg : IOperationsForOneArgument
+    public class Arctg : IOperationsForOneArgument
     {
         public double Calculate(double Argument)
         {
